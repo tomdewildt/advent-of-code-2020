fb4d89ae6572fccdeda58070787db2296d828395
tomdewildt <t.l.dewildt@gmail.com>
2020-12-03 23:22:09 +0100

    Add day 1 solution

---
+++ internal/day1/day1.go
@@ -0,0 +1,78 @@
+package day1
+
+import (
+	"errors"
+	"io"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+
+	"github.com/tomdewildt/advent-of-code-2020/pkg/cli"
+	"github.com/tomdewildt/advent-of-code-2020/pkg/input"
+)
+
+// AddCommandTo is used to add the command that exectutes the solve function to
+// the main command. This function is also used to load the challenge input. This
+// function takes the root command as an input. The result of the solve function
+// will be printed to stdout.
+func AddCommandTo(cmd *cobra.Command) {
+	cli.NewSubCommand(cmd, "1", func(cmd *cobra.Command, args []string) {
+		stream, err := input.FromFlags(cmd.Flags(), "file", "literal")
+		if err != nil {
+			log.Fatalf("cannot load: %v", err)
+		}
+
+		solution1, solution2, err := solve(stream)
+		if err != nil {
+			log.Fatalf("cannot solve: %v", err)
+		}
+
+		log.Infof("solution (1): %d", solution1)
+		log.Infof("solution (2): %d", solution2)
+	})
+}
+
+func solve(stream io.Reader) (int, int, error) {
+	input, err := input.ToIntSlice(stream)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	pair1, pair2, err := findPair(input, 2020)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	triple1, triple2, triple3, err := findTriple(input, 2020)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	return (pair1 * pair2), (triple1 * triple2 * triple3), nil
+}
+
+func findPair(input []int, target int) (int, int, error) {
+	cache := map[int]int{}
+
+	for index, value := range input {
+		if index, ok := cache[value]; ok {
+			return input[index], value, nil
+		}
+
+		cache[target-value] = index
+	}
+
+	return 0, 0, errors.New("pair not found")
+}
+
+func findTriple(input []int, target int) (int, int, int, error) {
+	for _, value := range input {
+		value1, value2, err := findPair(input, target-value)
+
+		if err == nil && value1 != value && value2 != value {
+			return value1, value2, value, nil
+		}
+	}
+
+	return 0, 0, 0, errors.New("triple not found")
+}
