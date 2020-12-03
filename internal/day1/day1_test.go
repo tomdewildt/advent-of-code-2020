e309fc2e9ccc7273de4f35d87bf9e4df23fdebb0
Tom de Wildt <32359630+tomdewildt@users.noreply.github.com>
2020-12-04 00:38:11 +0100

    Remove unused tests (#8)

--- internal/day1/day1_test.go
+++ internal/day1/day1_test.go
@@ -10,7 +10,7 @@ import (
 func TestSolveInvalidStream(t *testing.T) {
 	stream := input.FromLiteral("abc\n20\n1000\n1010\n1020\n")
 
-	solution1, solution2, err := solve(stream)
+	solution1, solution2, err := Solve(stream)
 
 	assert.Equal(t, 0, solution1, "Solution 1 should be 0")
 	assert.Equal(t, 0, solution2, "Solution 2 should be 0")
@@ -20,7 +20,7 @@ func TestSolveInvalidStream(t *testing.T) {
 func TestSolveNoPair(t *testing.T) {
 	stream := input.FromLiteral("10\n20\n1000\n1010\n")
 
-	solution1, solution2, err := solve(stream)
+	solution1, solution2, err := Solve(stream)
 
 	assert.Equal(t, 0, solution1, "Solution 1 should be 0")
 	assert.Equal(t, 0, solution2, "Solution 2 should be 0")
@@ -30,7 +30,7 @@ func TestSolveNoPair(t *testing.T) {
 func TestSolveNoTriple(t *testing.T) {
 	stream := input.FromLiteral("20\n1000\n1010\n1020\n")
 
-	solution1, solution2, err := solve(stream)
+	solution1, solution2, err := Solve(stream)
 
 	assert.Equal(t, 0, solution1, "Solution 1 should be 0")
 	assert.Equal(t, 0, solution2, "Solution 2 should be 0")
@@ -40,43 +40,9 @@ func TestSolveNoTriple(t *testing.T) {
 func TestSolve(t *testing.T) {
 	stream := input.FromLiteral("10\n20\n1000\n1010\n1020\n")
 
-	solution1, solution2, err := solve(stream)
+	solution1, solution2, err := Solve(stream)
 
 	assert.Equal(t, 1020000, solution1, "Solution 1 should be 1020000")
 	assert.Equal(t, 10100000, solution2, "Solution 2 should be 10100000")
-	assert.Nil(t, err, "Error should be nil")
-}
-
-func TestFindPairNotFound(t *testing.T) {
-	value1, value2, err := findPair([]int{10, 20, 30, 40}, 10)
-
-	assert.Equal(t, 0, value1, "Value 1 should be 0")
-	assert.Equal(t, 0, value2, "Value 2 should be 0")
-	assert.Equal(t, "pair not found", err.Error(), "Error should be \"pair not found\"")
-}
-
-func TestFindPair(t *testing.T) {
-	value1, value2, err := findPair([]int{1, 2, 3, 4, 5}, 8)
-
-	assert.Equal(t, 3, value1, "Value 1 should be 3")
-	assert.Equal(t, 5, value2, "Value 2 should be 5")
-	assert.Nil(t, err, "Error should be nil")
-}
-
-func TestFindTripleNotFound(t *testing.T) {
-	value1, value2, value3, err := findTriple([]int{10, 20, 30, 40}, 10)
-
-	assert.Equal(t, 0, value1, "Value 1 should be 0")
-	assert.Equal(t, 0, value2, "Value 2 should be 0")
-	assert.Equal(t, 0, value3, "Value 3 should be 0")
-	assert.Equal(t, "triple not found", err.Error(), "Error should be \"triple not found\"")
-}
-
-func TestFindTriple(t *testing.T) {
-	value1, value2, value3, err := findTriple([]int{1, 2, 3, 4, 5}, 8)
-
-	assert.Equal(t, 3, value1, "Value 1 should be 3")
-	assert.Equal(t, 4, value2, "Value 2 should be 4")
-	assert.Equal(t, 1, value3, "Value 3 should be 1")
 	assert.Nil(t, err, "Error should be nil")
 }
