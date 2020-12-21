78094e7cee9bd77b361577972c9f19a6c8f1d3d1
tomdewildt <t.l.dewildt@gmail.com>
2020-12-21 20:29:05 +0100

    Cleanup day 9 solution

--- internal/day9/day9_test.go
+++ internal/day9/day9_test.go
@@ -8,10 +8,32 @@ import (
 	"github.com/tomdewildt/advent-of-code-2020/pkg/input"
 )
 
-func TestSolveInvalidStream(t *testing.T) {
-	stream := input.FromLiteral("abc\n20\n30\n40\n50\n")
+const numbers = `35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576`
 
-	solution1, solution2, err := Solve(stream)
+func TestSolveInvalidNumber(t *testing.T) {
+	stream := input.FromLiteral("abc")
+	preamble := 5
+
+	solution1, solution2, err := Solve(stream, preamble)
 
 	assert.Equal(t, 0, solution1, "Solution 1 should be 0")
 	assert.Equal(t, 0, solution2, "Solution 2 should be 0")
@@ -19,11 +41,12 @@ func TestSolveInvalidStream(t *testing.T) {
 }
 
 func TestSolve(t *testing.T) {
-	stream, err := input.FromFile("../../assets/day9/day9.txt")
+	stream := input.FromLiteral(numbers)
+	preamble := 5
 
-	solution1, solution2, err := Solve(stream)
+	solution1, solution2, err := Solve(stream, preamble)
 
-	assert.Equal(t, 88311122, solution1, "Solution 1 should be 88311122")
-	assert.Equal(t, 13549369, solution2, "Solution 2 should be 13549369")
+	assert.Equal(t, 127, solution1, "Solution 1 should be 127")
+	assert.Equal(t, 62, solution2, "Solution 2 should be 62")
 	assert.Nil(t, err, "Error should be nil")
 }
