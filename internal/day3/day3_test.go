44469dd0e62316e6d88b268b811dfd83abf03dec
Tom de Wildt <32359630+tomdewildt@users.noreply.github.com>
2020-12-21 20:09:03 +0100

    Cleanup day 3 solution (#26)

--- internal/day3/day3_test.go
+++ internal/day3/day3_test.go
@@ -8,12 +8,24 @@ import (
 	"github.com/tomdewildt/advent-of-code-2020/pkg/input"
 )
 
+const tileMap = `..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#`
+
 func TestSolve(t *testing.T) {
-	stream := input.FromLiteral("..##...\n#..#..#\n.#...#.\n..#.#.#\n.#.#.##\n#......\n#.##..#\n")
+	stream := input.FromLiteral(tileMap)
 
 	solution1, solution2, err := Solve(stream)
 
-	assert.Equal(t, 3, solution1, "Solution 1 should be 3")
-	assert.Equal(t, 36, solution2, "Solution 2 should be 36")
+	assert.Equal(t, 7, solution1, "Solution 1 should be 7")
+	assert.Equal(t, 336, solution2, "Solution 2 should be 336")
 	assert.Nil(t, err, "Error should be nil")
 }
