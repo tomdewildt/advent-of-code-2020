1fec42118676745a07af214bcbd752eb97f9323d
Tom de Wildt <32359630+tomdewildt@users.noreply.github.com>
2020-12-21 20:03:48 +0100

    Cleanup day 1 solution (#24)

--- internal/day1/day1.go
+++ internal/day1/day1.go
@@ -1,7 +1,6 @@
 package day1
 
 import (
-	"errors"
 	"io"
 
 	log "github.com/sirupsen/logrus"
@@ -9,6 +8,7 @@ import (
 
 	"github.com/tomdewildt/advent-of-code-2020/pkg/cli"
 	"github.com/tomdewildt/advent-of-code-2020/pkg/input"
+	"github.com/tomdewildt/advent-of-code-2020/pkg/utils"
 )
 
 // AddCommandTo is used to add the command that exectutes the Solve function to
@@ -32,50 +32,24 @@ func AddCommandTo(cmd *cobra.Command) {
 	})
 }
 
-// Solve is used to find the solution to the problem. This function takes an stream
+// Solve is used to find the solution to the problem. This function takes a stream
 // of type io.Reader as input. It returns two integers and nil or 0, 0 and an error
 // if one occurred.
 func Solve(stream io.Reader) (int, int, error) {
-	input, err := input.ToIntSlice(stream)
+	expenses, err := input.ToIntSlice(stream)
 	if err != nil {
 		return 0, 0, err
 	}
 
-	pair1, pair2, err := findPair(input, 2020)
+	pair1, pair2, err := utils.FindPair(expenses, 2020)
 	if err != nil {
 		return 0, 0, err
 	}
 
-	triple1, triple2, triple3, err := findTriple(input, 2020)
+	triple1, triple2, triple3, err := utils.FindTriple(expenses, 2020)
 	if err != nil {
 		return 0, 0, err
 	}
 
 	return (pair1 * pair2), (triple1 * triple2 * triple3), nil
-}
-
-func findPair(input []int, target int) (int, int, error) {
-	cache := map[int]int{}
-
-	for index, value := range input {
-		if index, ok := cache[value]; ok {
-			return input[index], value, nil
-		}
-
-		cache[target-value] = index
-	}
-
-	return 0, 0, errors.New("pair not found")
-}
-
-func findTriple(input []int, target int) (int, int, int, error) {
-	for _, value := range input {
-		value1, value2, err := findPair(input, target-value)
-
-		if err == nil && value1 != value && value2 != value {
-			return value1, value2, value, nil
-		}
-	}
-
-	return 0, 0, 0, errors.New("triple not found")
 }
--- internal/day1/day1_test.go
+++ internal/day1/day1_test.go
@@ -8,8 +8,8 @@ import (
 	"github.com/tomdewildt/advent-of-code-2020/pkg/input"
 )
 
-func TestSolveInvalidStream(t *testing.T) {
-	stream := input.FromLiteral("abc\n20\n1000\n1010\n1020\n")
+func TestSolveInvalidValue(t *testing.T) {
+	stream := input.FromLiteral("abc\n979\n366\n299\n675\n1456")
 
 	solution1, solution2, err := Solve(stream)
 
@@ -19,31 +19,34 @@ func TestSolveInvalidStream(t *testing.T) {
 }
 
 func TestSolveNoPair(t *testing.T) {
-	stream := input.FromLiteral("10\n20\n1000\n1010\n")
+	stream := input.FromLiteral("1720\n979\n366\n299\n675\n1456")
 
 	solution1, solution2, err := Solve(stream)
 
 	assert.Equal(t, 0, solution1, "Solution 1 should be 0")
 	assert.Equal(t, 0, solution2, "Solution 2 should be 0")
-	assert.Contains(t, err.Error(), "pair not found", "Error should contain \"pair not found\"")
+	assert.Contains(t, err.Error(), "pair", "Error should contain \"triple\"")
+	assert.Contains(t, err.Error(), "not found", "Error should contain \"not found\"")
+
 }
 
 func TestSolveNoTriple(t *testing.T) {
-	stream := input.FromLiteral("20\n1000\n1010\n1020\n")
+	stream := input.FromLiteral("1721\n978\n366\n299\n675\n1456")
 
 	solution1, solution2, err := Solve(stream)
 
 	assert.Equal(t, 0, solution1, "Solution 1 should be 0")
 	assert.Equal(t, 0, solution2, "Solution 2 should be 0")
-	assert.Contains(t, err.Error(), "triple not found", "Error should contain \"triple not found\"")
+	assert.Contains(t, err.Error(), "triple", "Error should contain \"triple\"")
+	assert.Contains(t, err.Error(), "not found", "Error should contain \"not found\"")
 }
 
 func TestSolve(t *testing.T) {
-	stream := input.FromLiteral("10\n20\n1000\n1010\n1020\n")
+	stream := input.FromLiteral("1721\n979\n366\n299\n675\n1456")
 
 	solution1, solution2, err := Solve(stream)
 
-	assert.Equal(t, 1020000, solution1, "Solution 1 should be 1020000")
-	assert.Equal(t, 10100000, solution2, "Solution 2 should be 10100000")
+	assert.Equal(t, 514579, solution1, "Solution 1 should be 514579")
+	assert.Equal(t, 241861950, solution2, "Solution 2 should be 241861950")
 	assert.Nil(t, err, "Error should be nil")
 }
